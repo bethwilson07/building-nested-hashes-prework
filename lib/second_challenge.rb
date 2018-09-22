0fafc6b080ec60a96a7eebe00b85c70fa0a8bc1f
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-22 19:54:43 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,17 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-      }, 
-    :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
-      }
-    
-  }
-
+   }
+}
   
 end
